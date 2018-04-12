7fbf19009b2ae45fa32125a172a60bb23e5b27b9
higuchi <higuchi.dev@gmail.com>
2018-04-12 22:02:12 +0900

    no message

---
+++ Unity2DActionExample/Assets/GroundScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundScript : MonoBehaviour {
+
+    public bool visible = false;
+	// Use this for initialization
+	void Start () {
+        this.visible = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    
+    void OnBecameInvisible()
+    {
+        this.visible = false;
+    }
+
+    void OnBecameVisible()
+    {
+        this.visible = true;
+    }
+
+
+    public bool isVisible()
+    {
+        return this.visible;
+    }
+}
---
+++ Unity2DActionExample/Assets/LinePoint.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePoint {
+    public float point;
+    public int index;
+
+    public LinePoint()
+    {
+        this.point = 0.0f;
+        this.index = 0;
+    }
+}
